afdbf3068573a12092881b908ab5014070302106
programatextoc <cleberbezerra71@gmail.com>
2025-11-22 21:02:10 +0000

    Avançado

--- xadrez.c
+++ xadrez.c
@@ -4,75 +4,52 @@ if(Torre>0){
 moverTorre(Torre-1);
 printf("%d Movimento da Torre para direita:\n",Torre);}}
 
-int main() {
-//Movimento da torre 5 casas para direita FOR
-moverTorre(5);
+void moverRainha(int Rainha){
+if(Rainha>0){
+moverRainha(Rainha-1);
+printf("%d Movimento da rainha para esquerda:\n",Rainha);}}
 
+void moverBispo(int Bispo, int direita){
+if(Bispo>0,direita>0){
+moverBispo(Bispo-1,direita-1);
+printf("%d Movimento do Bispo superior diagonal,%d direita:\n",Bispo,direita);}}
+
+void moverCavalo(){
+int verticais=2;
+int horizontais=1;
+
+printf("Movimento do cavalo com break e continue(2 casas para baixo, 1 para esquerda:)\n\n");
+printf("Movimento 1: Movimento vertical(%d casas)\n",verticais);
+
+//simulação movimento vertical
+for(int vertical=1;vertical<=verticais;vertical++){
+printf("%d:Movimento para baixo\n",vertical);}
+
+printf("\n**********************************************\n");
+
+//simulação movimento horizontal
+printf("Movimento 2:(%d casa perpendicular) Movimento horizontal para esquerda\n",horizontais);
+for(int horizontal=1;horizontal<=5;horizontal++){
+if(horizontal>horizontais){
+printf("\n************************************************\n");
+printf("[break:O movimento horizontal de 1 passo foi concluido. sair]\n");
+
+break;}}}
+
+int main(){
+//Movimento da torre 5 casas para direita recursiva:
+moverTorre(5);
 printf("\n*************************************\n");
+
+//Movimento da Rainha 8 casas para esquerda Recursiva:
+moverRainha(8);
+printf("\n*****************************************\n");
 
 //Movimento do bispo 5 casas na diagonal a direita
-int i=1;
-while(i<=5){
-printf("Movimento do Bispo:casa %d na diagonal para cima/direita\n",i);
-i++;}
+moverBispo(5,5);
+printf("*******************************************\n");
 
-printf("\n*************************************\n");
-
-//reiniciar o 'i' para começar a contagem do 1
-i=1;
-
-//Movimento da rainha oito casas a esquerda
-do{
-printf("Movimento da Rainha:%d oito casas para esquerda\n",i);
-i++;} 
-while(i<=8);
-printf("\n*************************************\n");
-
-//Movimento do cavalo FOR
-int CasasParaBaixo=2;
-int CasasParaEsquerda=1;
-
-printf("Movimento do cavalo:L em FOR 2 casas para baixo e +1 para esquerda\n");
-
-printf("\n**************************\n");
-
-printf("Movimento 1: Movimento vertical(2 casas)\n");
-for(int x=1; x<=CasasParaBaixo; x++){
-printf("%d: Movimentos para baixo\n",x);
-if(x==CasasParaBaixo){
-
-printf("\n*************************************\n");
-
-printf("Movimento 2: Movimento horizontal(1 casa perpendicular)\n");
-for( int z=0; z<CasasParaEsquerda; z++){
-printf("Passo final: Movendo para esquerda\n");}}}
-
-
-printf("Movimento do cavalo em FOR completo\n");
-printf("\n***************************************\n");
-
-//Movimento do cavalo while
-int casasparabaixo=2;
-int casaparaesquerda=1;
-
-printf("Movimento do cavalo:L em WHILE 2 casas para baixo e +1 para esquerda\n");
-printf("\n**************************\n");
-
-printf("Movimento 1: Movimento vertical(2 casas)\n");
-int a=1;
-while(a<=casasparabaixo){
-printf("%d: Movimentos para baixo\n",a);
-if(a==CasasParaBaixo){
-printf("\n*******************************************\n");
-
-printf("Movimento 2: Movimento horizontal(1 casa perpendicular)\n");
-int b=0;
-while(b<casaparaesquerda){
-printf("Passo final: Movendo para esquerda\n");
-b++;}}
-a++;}
-printf("\n************************************\n");
-printf("Movimento do cavalo WHILE completo\n");
-printf("\n************************************\n");
-
+//Movimento do cavalo em L:
+moverCavalo();
+printf("\n*****************************************\n");
 return 0;}
